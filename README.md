182d8c6080b48d8391a3a014187984dc8f103f00
sixtusagbo <miracleagbosixtus@gmail.com>
2023-05-05 14:03:07 +0100

    Display a website

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# webview_codelab
+# Webview Codelab
 
 Flutter project I use to learn how to add and customize webview in flutter application.
 I followed this [codelab](https://codelabs.developers.google.com/codelabs/flutter-webview), you can check it out.
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,57 +11,39 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Webview',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Learn Flutter Webview',
+      theme: ThemeData(useMaterial3: true),
+      home: const WebViewApp(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<WebViewApp> createState() => _WebViewAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _WebViewAppState extends State<WebViewApp> {
+  late final WebViewController controller;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..loadRequest(Uri.parse('https://flutter.dev'));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Flutter WebView'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: WebViewWidget(
+        controller: controller,
       ),
     );
   }
