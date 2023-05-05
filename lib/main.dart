4f91446c7c6274755f02058e0086e5e358ee3259
sixtusagbo <miracleagbosixtus@gmail.com>
2023-05-05 14:15:13 +0100

    Listen to page load events

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'src/web_view_stack.dart';
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -27,24 +26,13 @@ class WebViewApp extends StatefulWidget {
 }
 
 class _WebViewAppState extends State<WebViewApp> {
-  late final WebViewController controller;
-
-  @override
-  void initState() {
-    super.initState();
-    controller = WebViewController()
-      ..loadRequest(Uri.parse('https://flutter.dev'));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter WebView'),
       ),
-      body: WebViewWidget(
-        controller: controller,
-      ),
+      body: const WebViewStack(),
     );
   }
 }
